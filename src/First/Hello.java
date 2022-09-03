d11aab51d9a24dd14d819eb8ffe95ff5736d2fdd
91910 <91910@DESKTOP-GFCRN3J>
2022-09-03 10:01:00 +0530

    THIS IS MY FIRST COMMIT

--- src/First/Hello.java
+++ src/First/Hello.java
@@ -8,7 +8,5 @@ public class Hello
 		System.out.println("Hello World!");
 		System.out.println("My name is khanija");
 		System.out.println("I started the training on Edubridge platform");
-
 	}
-
 } 
